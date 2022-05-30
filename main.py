16b5d9921201ae0a63e052114b68eeb5390646fa
Pliszka <karoliniakkjk@gmail.com>
2022-05-30 14:59:32 +0200

    saving new post

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
+import datetime as dt
 
-
-## Delete this code:
-# import requests
-# posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -38,7 +35,7 @@ class CreatePostForm(FlaskForm):
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     author = StringField("Your Name", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = StringField("Blog Content", validators=[DataRequired()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
 
@@ -67,9 +64,22 @@ def contact():
 def edit_post(post_id):
     return redirect(url_for('show_post', post_id=post_id))
 
-@app.route("/new-post")
+@app.route("/new-post", methods=["GET", "POST"])
 def new_post():
     form = CreatePostForm()
+    if form.validate_on_submit():
+        time_now = dt.datetime.now()
+        post = BlogPost(
+            title=request.form.get('title'),
+            subtitle=request.form.get('subtitle'),
+            date=f'{time_now.strftime("%A")} {time_now.day}, {time_now.year}',
+            body=request.form.get('body'),
+            author=request.form.get('author'),
+            img_url=request.form.get('img_url'),
+        )
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('get_all_posts'))
     return render_template('make-post.html', form=form)
 
 if __name__ == "__main__":
